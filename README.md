fa4c9d30c821e1cc6a5cef7ee29cb9c967a5a0d9
Jofelid <jofelid@3chats.ovh>
2021-06-07 20:15:01 +0200

    update readme + add comments

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # scrapBookPrices
+
+scrapBookPrices is a script to extract books prices from [Book to Scrape](http://books.toscrape.com/), a bookstore website.
+
+For each book category, the script will retrieve the following information:
+
+* Product Page URL
+* Universal Product Code (UPC)
+* Title
+* Price Including Tax
+* Price Excluding Tax
+* Number Available
+* Product Description
+* Category
+* Review Rating
+* Image url
+
+As well as the books covers.
+
+## Installation
+
+In a terminal:
+
+* Get project data.
+```bash
+git clone https://github.com/Jofelid/scrapBookPrices
+```
+
+* In the project folder, create then activate a virtual environment.
+```bash
+cd scrapBookPrices/
+python -m venv env
+source env/bin/activate
+```
+
+* Use the package manager [pip](https://pip.pypa.io/en/stable/) with the help of the requirement file to install dependencies.
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+In a terminal:
+
+* Run the script.
+```bash
+python main.py
+```
+
+A `downloads` folder will be create.
+In which, for each book category, there wille be :
+* a `[category].csv` file (delimiter : "**;**" )
+* a `[category]` folder containing the books covers
--- helpers.py
+++ helpers.py
@@ -5,6 +5,12 @@ import csv
 
 
 def get_categories(url):
+    """get_categories retrieves categories existing on a bookstore website
+
+    :param url: main page url
+    :return: a list of categories
+    """
+
     categories = []
 
     r = requests.get(url)
@@ -22,10 +28,14 @@ def get_categories(url):
 
 
 def get_books_from_category(url_category):
+    """get_books_from_category retrieves books existing on a category
+
+    :param url_category: category page url
+    :return: a list of books
+    """
     books_from_category = []
 
     r = requests.get(url_category)
-    i = 2
     while r.ok:
         soup = BeautifulSoup(r.content, 'html.parser')
 
@@ -34,14 +44,22 @@ def get_books_from_category(url_category):
             url_book = urljoin(url_category, book.find('a')['href'])
             books_from_category.append(url_book)
 
-        url_next = urljoin(url_category, 'page-' + str(i) + '.html')
+        # search for a new page for the category
+        pager_next = soup.find('li', {'class': 'next'})
+        pager_next_url = pager_next.a['href'] if pager_next else '.html'
+        url_next = urljoin(url_category, pager_next_url)
         r = requests.get(url_next)
-        i += 1
 
     return books_from_category
 
 
 def get_book_data(url_book, category):
+    """get_book_data retrieves book data
+
+    :param url_book: book page url
+    :param category: title of the category
+    :return: a dictionary containing the book data
+    """
     book_data = []
 
     r = requests.get(url_book)
@@ -49,10 +67,13 @@ def get_book_data(url_book, category):
         soup = BeautifulSoup(r.content, 'html.parser')
 
         title = soup.find('div', {'class': 'product_main'}).h1.text
+
+        # make a exception check when the product description doesn't exist
         product_description_p = soup.find('article').find('p', recursive=False)
-        product_description = product_description_p.text if product_description_p else ""
+        product_description = product_description_p.text if product_description_p else ''
 
         review_rating = soup.find('p', {'class': 'star-rating'})['class'][1]
+
         image_url = urljoin(url_book, soup.find('div', {'class': 'thumbnail'}).img['src'])
 
         table = {}
@@ -82,6 +103,12 @@ def get_book_data(url_book, category):
 
 
 def write_csv(file_name, data):
+    """Create a csv file containing multiple books data
+
+    :param file_name:path and name of the csv file
+    :param data: book dictionary data
+    :return: None
+    """
     with open(file_name, 'w') as csv_file:
         headers = data[0].keys()
         writer = csv.DictWriter(csv_file, headers, delimiter=';')
@@ -90,6 +117,12 @@ def write_csv(file_name, data):
 
 
 def download_image(url_image, file_name):
+    """Download a image
+
+    :param url_image: url of the image
+    :param file_name: path and name of the image file
+    :return: None
+    """
     r = requests.get(url_image, stream=True)
     if r.ok:
         with open(file_name, 'wb') as image_file:
